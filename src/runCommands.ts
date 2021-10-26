9d902281ab265d6c7bd70e3c5a0dafd901895c41
SSYino <gianmario.nino@gmail.com>
2021-10-26 21:53:45 +0700

    Create runCommands.ts

---
+++ src/runCommands.ts
@@ -0,0 +1,5 @@
+import deploySlashCommands from './commands/deploySlashCommands';
+import removeSlashCommands from './commands/removeSlashCommands';
+import commandInteractions from './commands/commandInteractions';
+
+export default { deploySlashCommands, removeSlashCommands, commandInteractions }
