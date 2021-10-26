6b7f951c39c290209b7420c552f617a159e9493a
SSYino <gianmario.nino@gmail.com>
2021-10-26 21:47:08 +0700

    Create deploySlashCommands.ts

---
+++ src/commands/deploySlashCommands.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export default async (msg: any, client: any): Promise<void> => {
+    const clientApp = await client.application.fetch(client.application.id);
+
+    if (msg.author.id !== clientApp.owner.id) return;
+
+    await client.guilds.cache.get(msg.guild.id)?.commands.set([
+        new SlashCommandBuilder()
+            .setName("start")
+            .setDescription("Start the spy game \"The Peculiars\"")
+            .setDefaultPermission(true),
+
+        new SlashCommandBuilder()
+            .setName("begin")
+            .setDescription("Begin playing the \"The Peculiars\"")
+            .setDefaultPermission(false),
+
+        new SlashCommandBuilder()
+            .setName("end")
+            .setDescription("End the spy game \"The Peculiars\"")
+            .setDefaultPermission(false),
+
+        new SlashCommandBuilder()
+            .setName("ping")
+            .setDescription("Pong")
+            .setDefaultPermission(true)
+    ]);
+    await msg.reply("🥷 | Deployed");
+}
