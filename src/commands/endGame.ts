2f4b93813a379473cfc4fc6bed531723ca058575
SSYino <gianmario.nino@gmail.com>
2021-10-28 23:42:29 +0700

    Update endGame.ts

--- src/commands/endGame.ts
+++ src/commands/endGame.ts
@@ -1,24 +1,25 @@
 import { ApplicationCommand, Collection, CommandInteraction, MessageEmbed, Role, TextChannel } from "discord.js";
+import { promisify } from "util";
 
 export default async (interaction: CommandInteraction, event_interaction: CommandInteraction, inviteMessageEmebed: MessageEmbed, slashCommands: Collection<string, ApplicationCommand>, everyoneRole: Role, playerRole: Role, gameChannel: TextChannel) => {
     // Lock "begin" and "end" slash commands from being used
     for (const command of slashCommands) {
         if (command[1].name === 'begin' || command[1].name === 'end') {
             await slashCommands
-            .get(command[1].id)
-            ?.permissions
-            .set({
-                permissions: [
-                    {
-                        id: playerRole.id,
-                        type: "ROLE",
-                        permission: false
-                    }
-                ]
-            })
+                .get(command[1].id)
+                ?.permissions
+                .set({
+                    permissions: [
+                        {
+                            id: playerRole.id,
+                            type: "ROLE",
+                            permission: false
+                        }
+                    ]
+                })
         }
     }
-    
+
     // Change Invite Message Embed
     const newInviteMessageEmebed = new MessageEmbed(inviteMessageEmebed);
     const [fieldPlayerCount, fieldGameRound, fieldGameStatus] = newInviteMessageEmebed.fields;
@@ -26,13 +27,12 @@ export default async (interaction: CommandInteraction, event_interaction: Comman
     fieldPlayerCount.value = fieldPlayerCount.value.match(/^\d+/)![0];
     fieldGameRound.name = "**Rounds Played**";
     fieldGameStatus.value = "Terminated";
-    interaction.editReply({embeds: [newInviteMessageEmebed]});
+    interaction.editReply({ embeds: [newInviteMessageEmebed] });
 
     await event_interaction.followUp("**Deleting this channel in 5 seconds**")
-    setTimeout(() => {
-        gameChannel.delete();
-        playerRole.delete();
-    }, 5000)
+    await promisify(setTimeout)(5000)
+    playerRole.delete();
+    gameChannel.delete();
 
     // Allow the use of the "start" slash command to start a new game
     slashCommands.find(command => command.name === "start")?.permissions.set({ permissions: [{ id: everyoneRole.id, type: "ROLE", permission: true }] })
