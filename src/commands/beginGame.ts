6187241cc944dc7ad5b0d4fb541edd93b797f23f
SSYino <gianmario.nino@gmail.com>
2021-10-28 15:36:01 +0700

    Update beginGame.ts
    Disable "begin" Slash Command
    Update Invite Message

--- src/commands/beginGame.ts
+++ src/commands/beginGame.ts
@@ -1,3 +1,19 @@
-export default async () => {
-    
+import { CommandInteraction, MessageEmbed, ApplicationCommand } from "discord.js";
+
+export default async (inviteMessageInteraction: CommandInteraction, gameInteraction: CommandInteraction, inviteMessageEmbed: MessageEmbed) => {
+    // Disable "begin" Slash Command
+    const beginSlashCommand = gameInteraction.guild?.commands.cache.find(command => command.name === 'begin')
+    if (!beginSlashCommand) return gameInteraction.channel?.send("Error: Could not find \"begin\" Slash Command in cache\nbeginGame.ts")
+    beginSlashCommand.permissions.set({permissions: []})
+
+    // Update invite message
+    const newEmbed = new MessageEmbed(inviteMessageEmbed)
+    const [fieldPlayerCount, fieldRound, fieldGameStatus] = newEmbed.fields
+    const currentPlayerCount = fieldPlayerCount.value.match(/^\d+/)![0];
+
+    fieldPlayerCount.name = "**Playing**";
+    fieldPlayerCount.value = currentPlayerCount;
+    fieldGameStatus.value = "In Progress";
+
+    inviteMessageInteraction.editReply({ embeds: [newEmbed] })
 }
