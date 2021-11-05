2fd4890c7797c87c18dbc073a6ae8e5daa5cf278
SSYino <gianmario.nino@gmail.com>
2021-11-05 12:38:30 +0700

    Update endGame.ts

--- src/commands/endGame.ts
+++ src/commands/endGame.ts
@@ -1,7 +1,7 @@
-import { ApplicationCommand, Collection, CommandInteraction, MessageEmbed, Role, TextChannel } from "discord.js";
+import { ApplicationCommand, Collection, CommandInteraction, Message, MessageEmbed, Role, TextChannel } from "discord.js";
 import { promisify } from "util";
 
-export default async (interaction: CommandInteraction, event_interaction: CommandInteraction, inviteMessageEmebed: MessageEmbed, slashCommands: Collection<string, ApplicationCommand>, everyoneRole: Role, playerRole: Role, gameChannel: TextChannel) => {
+export default async (inviteMessage: Message, event_interaction: CommandInteraction, slashCommands: Collection<string, ApplicationCommand>, everyoneRole: Role, playerRole: Role, gameChannel: TextChannel) => {
     // Lock "begin" and "end" slash commands from being used
     for (const command of slashCommands) {
         if (command[1].name === 'begin' || command[1].name === 'end') {
@@ -21,18 +21,21 @@ export default async (interaction: CommandInteraction, event_interaction: Comman
     }
 
     // Change Invite Message Embed
-    const newInviteMessageEmebed = new MessageEmbed(inviteMessageEmebed);
+    const oldInviteMessageEmbed = (await inviteMessage.fetch()).embeds[0];
+    const newInviteMessageEmebed = new MessageEmbed(oldInviteMessageEmbed);
     const [fieldPlayerCount, fieldGameRound, fieldGameStatus] = newInviteMessageEmebed.fields;
     fieldPlayerCount.name = "**Participants**";
     fieldPlayerCount.value = fieldPlayerCount.value.match(/^\d+/)![0];
     fieldGameRound.name = "**Rounds Played**";
     fieldGameStatus.value = "Terminated";
-    interaction.editReply({ embeds: [newInviteMessageEmebed] });
+    inviteMessage.edit({ embeds: [newInviteMessageEmebed] });
 
-    await event_interaction.followUp("**Deleting this channel in 5 seconds**")
-    await promisify(setTimeout)(5000)
-    playerRole.delete();
-    gameChannel.delete();
+    try {
+        await event_interaction.followUp("**Deleting this channel in 5 seconds**")
+        await promisify(setTimeout)(5000)
+        await playerRole.delete();
+        await gameChannel.delete();
+    } catch {console.error("Error: Could not delete GameRole/GameChannel -- Possible that more than one player tried to end the game at the same time")}
 
     // Allow the use of the "start" slash command to start a new game
     slashCommands.find(command => command.name === "start")?.permissions.set({ permissions: [{ id: everyoneRole.id, type: "ROLE", permission: true }] })
