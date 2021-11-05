93c6b7d50463af004d138c88f9239c558bde6f13
SSYino <gianmario.nino@gmail.com>
2021-11-05 12:51:46 +0700

    Create ready.ts

---
+++ src/utils/buttons/ready.ts
@@ -0,0 +1,19 @@
+import { MessageActionRow, MessageButton } from "discord.js"
+
+export default ((state: ("waiting" | "started")): MessageActionRow => {
+    let button = (component: MessageButton) => new MessageActionRow().addComponents(component);
+
+    switch (state) {
+        case "waiting":
+            return button(new MessageButton()
+                .setCustomId("ready")
+                .setLabel("READY")
+                .setStyle("PRIMARY"))
+        case "started":
+            return button(new MessageButton()
+            .setCustomId("ready")
+            .setLabel("Good Luck!")
+            .setStyle("SUCCESS")
+            .setDisabled(true))
+    }
+})
