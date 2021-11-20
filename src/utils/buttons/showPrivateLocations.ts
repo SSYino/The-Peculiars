5b6ac6b228f8c4f6cbcf2110ca6cf09e662e6804
SSYino <gianmario.nino@gmail.com>
2021-11-20 22:25:34 +0700

    Add disable functionality

--- src/utils/buttons/showPrivateLocations.ts
+++ src/utils/buttons/showPrivateLocations.ts
@@ -1,8 +1,9 @@
 import { MessageActionRow, MessageButton } from "discord.js"
 
-export default new MessageActionRow().addComponents(new MessageButton()
+export default (disable: boolean = false) => new MessageActionRow().addComponents(new MessageButton()
     .setCustomId("showPrivateLocations")
     .setLabel("Show Private Locations")
     .setStyle("PRIMARY")
     .setEmoji("🚩")
+    .setDisabled(disable)
 )
