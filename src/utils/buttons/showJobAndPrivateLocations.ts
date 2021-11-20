6c38a73146726b4f73f80d6409a744efc92ea153
SSYino <gianmario.nino@gmail.com>
2021-11-20 22:27:27 +0700

    Delete showJobAndPrivateLocations.ts

--- src/utils/buttons/showJobAndPrivateLocations.ts
+++
@@ -1,8 +0,0 @@
-import { MessageActionRow } from "discord.js";
-import showJob from "./showJob";
-import showPrivateLocations from "./showPrivateLocations";
-
-const showJobComponents = showJob.components;
-const showPrivateLocationsComponent = showPrivateLocations.components;
-
-export default new MessageActionRow().addComponents(showJobComponents, showPrivateLocationsComponent);
