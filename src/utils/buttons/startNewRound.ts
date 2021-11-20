636e1b8e7b282a880ea0af766a8f5b72d6b7fca1
SSYino <gianmario.nino@gmail.com>
2021-11-20 22:28:32 +0700

    Add disable functionality

--- src/utils/buttons/startNewRound.ts
+++ src/utils/buttons/startNewRound.ts
@@ -1,10 +1,11 @@
 import { MessageActionRow, MessageButton } from "discord.js";
 
-export default (): MessageActionRow => {
+export default (disable: boolean = false): MessageActionRow => {
     return new MessageActionRow().addComponents(new MessageButton()
         .setCustomId("startNewRound")
         .setLabel("Start The Next Round")
         .setStyle("PRIMARY")
         .setEmoji("⏭️")
+        .setDisabled(disable)
     )
 }
